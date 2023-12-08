f56b2458f3aa56c5e35595ff83b98b6e53f51788
XuboZheng <zhengxubo1006@qq.com>
2023-12-08 20:42:16 +0800

    代码风格配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,23 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': ['warn'],
+    'vue/multi-word-component-names': [
+      'warn',
+      {
+        ignores: ['index']
+      }
+    ],
+    'vue/no-setup-props-destructure': ['off'],
+    'no-undef': 'error'
   }
 }
