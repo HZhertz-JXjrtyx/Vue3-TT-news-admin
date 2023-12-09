245109e448c63700bde3a8f97b47d977ad01d1d1
XuboZheng <zhengxubo1006@qq.com>
2023-12-09 23:05:10 +0800

    pinia持久化

--- vite.config.js
+++ vite.config.js
@@ -18,18 +18,7 @@ export default defineConfig({
     postcss: {
       plugins: [
         postcsspxtoviewport({
-          unitToConvert: 'px',
-          viewportWidth: 750,
-          unitPrecision: 6,
-          propList: ['*'],
-          viewportUnit: 'vw',
-          fontViewportUnit: 'vw',
-          selectorBlackList: ['ignore-'],
-          minPixelValue: 1,
-          mediaQuery: true,
-          replace: true,
-          exclude: [],
-          landscape: false
+          viewportWidth: 750
         })
       ]
     }
