9ac7af3197be2bb28128b39321a87d8314e64095
XuboZheng <HZhertz76@gmail.com>
2024-04-09 12:46:40 +0800

    feat(ArticlePage): 文章内容渲染

--- src/views/article/ArticlePage.vue
+++ src/views/article/ArticlePage.vue
@@ -1,22 +1,32 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 import '@/styles/github-markdown-light.css'
-// import { convertToMMDDHHmm } from '@/utils/convert'
+import { getArticle } from '@/api'
+import { convertToMMDDHHmm } from '@/utils/convert'
 import ArticleComment from '@/components/article/ArticleComment.vue'
 
-defineProps({
+const props = defineProps({
   articleId: {
     type: String,
     required: true,
   },
 })
+const router = useRouter()
 
-const pubtime = ref('')
 const commentContent = ref('asddfhahjfajfjhahjf')
 
-onMounted(async () => {
+const articleInfo = ref({})
+const pubtime = ref('')
+const getArticleInfo = async () => {
+  const res = await getArticle({ article_id: props.articleId })
+  articleInfo.value = res.data
+  pubtime.value = convertToMMDDHHmm(articleInfo.value.publish_time)
+}
+
+onMounted(() => {
+  getArticleInfo()
   // await newsStore.getArticle(props.articleId)
-  // pubtime.value = convertToMMDDHHmm(newsStore.article.publish_time)
 })
 </script>
 
@@ -25,26 +35,24 @@ onMounted(async () => {
     <van-sticky
       ><van-nav-bar title="文章详情" class="app-nav-bar">
         <template #left>
-          <span class="iconfont icon-a-44tubiao-14" @click="$router.back()"></span>
+          <span class="iconfont icon-a-44tubiao-14" @click="router.back()"></span>
         </template> </van-nav-bar
     ></van-sticky>
 
     <div class="article">
-      <!-- <h1 class="title">{{ newsStore.article.title }}</h1> -->
+      <h1 class="title">{{ articleInfo.title }}</h1>
       <div class="user-info">
         <van-image
           class="user-avatar"
           round
           width="40px"
           height="40px"
-          :src="newsStore.article.user_info ? newsStore.article.user_info.user_avatar : ''"
+          :src="articleInfo.user_info ? articleInfo.user_info.user_avatar : ''"
         />
-        <span class="user-name">{{
-          newsStore.article.user_info ? newsStore.article.user_info.user_nickname : ''
-        }}</span>
+        <span class="user-name">{{ articleInfo.user_info ? articleInfo.user_info.user_nickname : '' }}</span>
         <span class="pub-time">{{ pubtime }}</span>
         <van-button
-          v-if="!newsStore.article.is_followed"
+          v-if="!articleInfo.is_followed"
           class="follow-btn"
           size="small"
           color="linear-gradient(to right, #ff6034, #ee0a24)"
@@ -56,7 +64,7 @@ onMounted(async () => {
       </div>
       <div
         class="article-content markdown-body"
-        v-html="newsStore.article.article_info ? newsStore.article.article_info.content : ''"
+        v-html="articleInfo.article_info ? articleInfo.article_info.content : ''"
       ></div>
     </div>
     <div class="divider"></div>
@@ -76,40 +84,41 @@ onMounted(async () => {
 
 <style lang="less" scoped>
 .article {
-  margin: 10px 10px 50px;
+  margin: 20px 20px 100px;
   .title {
-    margin-bottom: 10px;
-    font-size: 22px;
+    margin-bottom: 20px;
+    font-size: 42px;
   }
   .user-info {
     position: relative;
+    margin-bottom: 20px;
 
     .user-name {
       position: absolute;
-      left: 45px;
+      left: 90px;
     }
     .pub-time {
       position: absolute;
-      top: 24px;
-      left: 45px;
-      font-size: 14px;
+      top: 48px;
+      left: 90px;
+      font-size: 28px;
       color: var(--text-color-2);
     }
     .follow-btn {
       position: absolute;
-      top: 5px;
+      top: 10px;
       right: 0px;
     }
   }
 }
 .divider {
-  width: 375px;
-  height: 6px;
+  width: 750px;
+  height: 12px;
   margin: 0;
   background-color: var(--bg-color-3);
 }
 .comment {
-  margin: 10px 10px 80px;
+  margin: 20px 20px 160px;
 }
 .bottom {
   position: fixed;
@@ -118,22 +127,22 @@ onMounted(async () => {
   align-items: center;
   justify-content: space-between;
   width: 100%;
-  height: 50px;
-  padding: 0 20px;
+  height: 100px;
+  padding: 0 40px;
   background-color: var(--bg-color-1);
-  border-top: 1px solid var(--bg-color-3);
+  border-top: 2px solid var(--bg-color-3);
   .bottom-comment {
-    width: 140px;
-    height: 30px;
-    padding-left: 12px;
+    width: 280px;
+    height: 60px;
+    padding-left: 24px;
     overflow: hidden;
     text-overflow: ellipsis;
-    border: 1px solid var(--bg-color-2);
-    border-radius: 15px;
+    border: 2px solid var(--bg-color-2);
+    border-radius: 30px;
     background-color: var(--bg-color-3);
   }
   .iconfont {
-    font-size: 20px;
+    font-size: 40px;
   }
   .icon-a-44tubiao-188,
   .icon-a-44tubiao-242 {
