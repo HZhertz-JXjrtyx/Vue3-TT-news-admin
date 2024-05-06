52b451d5d4af30b0617763c7319bfd3dc348c4fd
XuboZheng <HZhertz76@gmail.com>
2024-05-06 17:20:07 +0800

    feat(MessageTab): 完成消息私信页面结构样式

--- src/views/MessageTab/index.vue
+++ src/views/MessageTab/index.vue
@@ -1,15 +1,66 @@
 <template>
-  <div class="title">消息私信</div>
-  <div class="notification">
-    <van-cell title="评论" is-link />
-    <van-cell title="点赞" is-link />
-    <van-cell title="粉丝" is-link />
+  <div class="tab-title">消息私信</div>
+  <div class="inform">
+    <van-cell is-link>
+      <template #icon>
+        <div class="inform-icon" :style="{ backgroundColor: '#00b2fd' }">
+          <img src="@/assets/image/comment_icon.png" alt="" />
+        </div>
+      </template>
+      <template #title>
+        <div class="inform-content">
+          <span class="content__type-text">评论</span>
+          <span class="content__latest-word">xxx评论了你的评论</span>
+        </div>
+      </template>
+      <template #value>
+        <div class="inform-badge">
+          <van-badge :content="100" max="99" />
+        </div>
+      </template>
+    </van-cell>
+    <van-cell is-link>
+      <template #icon>
+        <div class="inform-icon" :style="{ backgroundColor: '#ff676c' }">
+          <img src="@/assets/image/like_icon.png" alt="" />
+        </div>
+      </template>
+      <template #title>
+        <div class="inform-content">
+          <span class="content__type-text">点赞</span>
+          <span class="content__latest-word">xxx点赞了你的xx</span>
+        </div>
+      </template>
+      <template #value>
+        <div class="inform-badge">
+          <van-badge :content="100" max="99" />
+        </div>
+      </template>
+    </van-cell>
+    <van-cell is-link>
+      <template #icon>
+        <div class="inform-icon" :style="{ backgroundColor: '#ff7f21' }">
+          <img src="@/assets/image/fans_icon.png" alt="" />
+        </div>
+      </template>
+      <template #title>
+        <div class="inform-content">
+          <span class="content__type-text">粉丝</span>
+          <span class="content__latest-word">xxx关注了你</span>
+        </div>
+      </template>
+      <template #value>
+        <div class="inform-badge">
+          <van-badge :content="100" max="99" />
+        </div>
+      </template>
+    </van-cell>
   </div>
-  <div class="message-list">
-    <van-swipe-cell>
+  <div class="conversation-list">
+    <van-swipe-cell right-width="40">
       <div class="conversation">
-        <div class="conversation__left">
-          <div class="user-avatar">
+        <div class="conversation-left">
+          <div class="left__user-avatar">
             <van-image
               round
               fit="cover"
@@ -18,13 +69,13 @@
             />
           </div>
         </div>
-        <div class="conversation__center">
-          <div class="user-nickname">用户</div>
-          <div class="latest-word">eeadaffgagagagagagagafafafgageee</div>
+        <div class="conversation-center">
+          <span class="center__user-nickname">用户</span>
+          <span class="center__latest-word">eeadaffgagagagagagagafafafgageee</span>
         </div>
-        <div class="conversation__right">
-          <div class="latest-time">04-25</div>
-          <div class="badge"><van-badge :content="1" max="99" /></div>
+        <div class="conversation-right">
+          <span class="right__latest-time">04-25</span>
+          <span class="right__badge"><van-badge :content="100" max="99" /></span>
         </div>
       </div>
       <template #right>
@@ -36,22 +87,48 @@
 
 <script setup></script>
 <style lang="less" scoped>
-.notification {
-  border-bottom: 8px solid #f0f0f0;
-}
-.title {
+.tab-title {
   padding: 30px 30px;
   font-size: 34px;
   text-align: center;
   border-bottom: 1px solid #eeeeee;
 }
+.inform {
+  border-bottom: 8px solid #f0f0f0;
+  .inform-icon {
+    background-color: blue;
+    width: 100px;
+    height: 100px;
+    margin-right: 30px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+      width: 60%;
+    }
+  }
+  .inform-badge {
+    position: relative;
+    .van-badge {
+      position: absolute;
+      top: 70px;
+      right: 28px;
+    }
+  }
+  :deep(.van-cell__right-icon) {
+    line-height: 100px;
+  }
+}
+
 .conversation {
   display: flex;
   padding: 20px 30px;
-  .conversation__left {
-    .user-avatar {
-      width: 120px;
-      height: 120px;
+  border-bottom: 1px solid #efefef;
+  .conversation-left {
+    .left__user-avatar {
+      width: 100px;
+      height: 100px;
       margin-right: 30px;
       .van-image {
         width: 100%;
@@ -59,43 +136,63 @@
       }
     }
   }
-  .conversation__center {
+  .conversation-center {
     flex: 1;
-    .user-nickname {
-      margin-top: 16px;
-      font-size: 32px;
-      font-weight: bolder;
-    }
-    .latest-word {
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      width: 420px;
-      margin-top: 10px;
-      color: #686868;
-    }
   }
-  .conversation__right {
+  .conversation-right {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
     width: 100px;
     text-align: right;
-    .latest-time {
-      margin-top: 16px;
+    .right__latest-time {
+      font-size: 24px;
       color: #a8a8a8;
     }
-    .badge {
+    .right__badge {
       position: relative;
-      margin-top: 10px;
+      height: 40px;
       .van-badge {
         position: absolute;
         top: 20px;
-        right: 20px;
+        right: 28px;
       }
     }
   }
 }
+
 .van-swipe-cell__right {
+  width: 100px;
+  text-align: center;
   .van-button {
     height: 100%;
+    width: 100px;
+    padding: 0 30px 0 12px;
   }
+}
+
+.inform-content,
+.conversation-center {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+.content__type-text,
+.center__user-nickname {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 460px;
+  font-size: 30px;
+  font-weight: bolder;
+}
+.content__latest-word,
+.center__latest-word {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 460px;
+  font-size: 26px;
+  color: #686868;
 }
 </style>
