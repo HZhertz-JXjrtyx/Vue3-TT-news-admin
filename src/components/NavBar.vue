c32b23827141284870aca267eea8023342636c89
XuboZheng <HZhertz76@gmail.com>
2024-05-10 21:32:04 +0800

    feat(Navbar): 修改Navbar样式

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -17,8 +17,20 @@ const router = useRouter()
       <template #left>
         <span class="iconfont icon-arrow_left" @click="router.back()"></span>
       </template>
+      <template #right>
+        <span class="iconfont icon-home" @click="router.replace('/home')"></span>
+      </template>
     </van-nav-bar>
   </van-sticky>
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.app-nav-bar {
+  background-color: #fff;
+  border-bottom: 1px solid #d6d6d6;
+
+  :deep(.van-nav-bar__title) {
+    color: #000000;
+  }
+}
+</style>
