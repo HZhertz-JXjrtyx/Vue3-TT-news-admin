b93552263c4f40fb3716f9ceada541e8432cb0cb
XuboZheng <HZhertz76@gmail.com>
2024-05-11 13:04:26 +0800

    feat(ConversationDetail): 消息列表高度随发送栏的高度变化而变化

--- src/views/ConversationDetail/index.vue
+++ src/views/ConversationDetail/index.vue
@@ -58,74 +58,81 @@ const handleSend = async () => {
 </script>
 <template>
   <NavBar :title="interlocutor.user_nickname || ''" />
-  <div class="message-list">
-    <ul>
-      <li v-for="item in messageList" :key="item._id">
-        <div class="time-label" v-if="item.isTimeLabel">{{ formatMessageTime(item.time) }}</div>
-        <div
-          class="message-item"
-          :class="{ 'send-itself': item.sender._id === userStore.userInfo._id }"
-          v-else
-        >
-          <div class="user-avatar">
-            <van-image round fit="cover" position="center" :src="item.sender.user_avatar" />
+  <div class="container">
+    <div class="message-list">
+      <ul>
+        <li v-for="item in messageList" :key="item._id">
+          <div class="time-label" v-if="item.isTimeLabel">{{ formatMessageTime(item.time) }}</div>
+          <div
+            class="message-item"
+            :class="{ 'send-itself': item.sender._id === userStore.userInfo._id }"
+            v-else
+          >
+            <div class="user-avatar">
+              <van-image round fit="cover" position="center" :src="item.sender.user_avatar" />
+            </div>
+            <span class="message-content">{{ item.content }}</span>
           </div>
-          <span class="message-content">{{ item.content }}</span>
-        </div>
-      </li>
-    </ul>
-  </div>
-  <div class="bottom-send">
-    <van-field v-model="sendMessage" rows="1" autosize type="textarea" placeholder="发消息..." />
-    <span class="iconfont icon-image"></span>
-    <span class="send-icon" @click="handleSend">发送</span>
+        </li>
+      </ul>
+    </div>
+    <div class="bottom-send">
+      <van-field v-model="sendMessage" rows="1" autosize type="textarea" placeholder="发消息..." />
+      <span class="iconfont icon-image"></span>
+      <span class="send-icon" @click="handleSend">发送</span>
+    </div>
   </div>
 </template>
 
 <style lang="less" scoped>
-.message-list {
-  height: calc(100vh - 180px);
-  overflow: auto;
-}
-.time-label {
-  text-align: center;
-  font-size: 22px;
-  color: #909090;
-}
-.message-item {
+.container {
   display: flex;
-  margin: 20px;
-  .user-avatar {
-    width: 80px;
-    height: 80px;
-    .van-image {
-      width: 100%;
-      height: 100%;
-      border: 1px solid #f3f3f3;
+  flex-direction: column;
+  height: calc(100vh - 100px);
+}
+
+.message-list {
+  flex: 1;
+  overflow: auto;
+  .time-label {
+    text-align: center;
+    font-size: 22px;
+    color: #909090;
+  }
+  .message-item {
+    display: flex;
+    margin: 20px;
+    .user-avatar {
+      width: 80px;
+      height: 80px;
+      .van-image {
+        width: 100%;
+        height: 100%;
+        border: 1px solid #f3f3f3;
+      }
+    }
+    .message-content {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      max-width: 70%;
+      margin: 0 15px;
+      padding: 15px;
+      line-height: 1.5;
+      word-wrap: break-word;
+      border-radius: 10px;
+      background-color: #eaeaea;
     }
   }
-  .message-content {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    max-width: 70%;
-    margin: 0 15px;
-    padding: 15px;
-    line-height: 1.5;
-    word-wrap: break-word;
-    border-radius: 10px;
-    background-color: #eaeaea;
+  .send-itself {
+    flex-direction: row-reverse;
   }
 }
-.send-itself {
-  flex-direction: row-reverse;
-}
+
 .bottom-send {
-  background-color: #ffffff;
   display: flex;
   align-items: flex-end;
-  position: absolute;
-  bottom: 0;
+  background-color: #ffffff;
   border-top: 1px solid #e8e8e8;
   width: 100%;
   .van-field {
