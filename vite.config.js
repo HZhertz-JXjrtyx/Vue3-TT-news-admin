3bca7b218316c6f98076ab56bfa0ce49b1e4f2a4
XuboZheng <HZhertz76@gmail.com>
2024-05-27 12:44:49 +0800

    chore: 配置代理

--- vite.config.js
+++ vite.config.js
@@ -27,6 +27,12 @@ const quill_pxtovw = pxtovw({
 export default defineConfig({
   server: {
     proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3007',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+
       '/socket.io': {
         target: 'http://127.0.0.1:3007',
         changeOrigin: true,
